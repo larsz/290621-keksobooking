794536e1541a3fba0d1f3b663e85cd3e416165c9
larsz <eugen.lobicov@gmail.com>
2018-02-05 21:35:18 +0200

    Добавляет константы объектов

---
+++ js/map.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var OFFER_TITLE = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
+var OFFER_TYPE = [
+  'flat',
+  'house',
+  'bungalo'
+];
+var OFFER_CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+var OFFER_CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+var OFFER_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+var LOCATION_X_MIN = 300;
+var LOCATION_X_MAX = 900;
+var LOCATION_Y_MIN = 150;
+var LOCATION_Y_MAX = 500;
+var AVATAR_PATH = 'img/avatars/';
+var OFFER_PHOTO_PATH = 'http://o0.github.io/assets/images/tokyo/';
+
+
