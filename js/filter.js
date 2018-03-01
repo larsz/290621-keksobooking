01166089b4607cc3d733cbc620d7fe4711ab51f0
larsz <eugen.lobicov@gmail.com>
2018-03-01 12:17:35 +0200

    Выносит фильтры в модуль

---
+++ js/filter.js
@@ -0,0 +1,109 @@
+'use strict';
+
+(function () {
+  var formFiltersElement = document.querySelector('.map__filters');
+  var typeFilterValue = 'any';
+  var priceFilterValue = 'any';
+  var roomsFilterValue = 'any';
+  var guestsFilterValue = 'any';
+  var wifiFilterValue = false;
+  var dishwasherFilterValue = false;
+  var parkingFilterValue = false;
+  var washerFilterValue = false;
+  var elevatorFilterValue = false;
+  var conditionerFilterValue = false;
+
+  var Price = {
+    LOW: 10000,
+    HIGH: 50000
+  };
+
+  var onFilterChangeExternal = null;
+
+  var setCallback = function (cb) {
+    onFilterChangeExternal = cb;
+  };
+
+  var applyFilter = function (offers) {
+    return offers.filter(function (offer) {
+
+      var hasAppropiatePrice;
+      switch (priceFilterValue) {
+        case 'any':
+          hasAppropiatePrice = true;
+          break;
+        case 'low':
+          hasAppropiatePrice = offer.offer.price < Price.LOW;
+          break;
+        case 'middle':
+          hasAppropiatePrice = offer.offer.price >= Price.LOW && offer.offer.price <= Price.HIGH;
+          break;
+        case 'high':
+          hasAppropiatePrice = offer.offer.price > Price.HIGH;
+          break;
+      }
+
+      var hasAppropiateType = typeFilterValue === 'any' || offer.offer.type.toString() === typeFilterValue;
+      var hasAppropiateRooms = roomsFilterValue === 'any' || offer.offer.rooms.toString() === roomsFilterValue;
+      var hasAppropiateGuests = guestsFilterValue === 'any' || offer.offer.guests.toString() === guestsFilterValue;
+      var hasWiFi = wifiFilterValue === false || offer.offer.features.indexOf('wifi') !== -1;
+      var hasDishWasher = dishwasherFilterValue === false || offer.offer.features.indexOf('dishwasher') !== -1;
+      var hasParking = parkingFilterValue === false || offer.offer.features.indexOf('parking') !== -1;
+      var hasWasher = washerFilterValue === false || offer.offer.features.indexOf('washer') !== -1;
+      var hasElevator = elevatorFilterValue === false || offer.offer.features.indexOf('elevator') !== -1;
+      var hasConditioner = conditionerFilterValue === false || offer.offer.features.indexOf('conditioner') !== -1;
+
+      return hasAppropiateType && hasAppropiatePrice && hasAppropiateRooms && hasAppropiateGuests && hasWiFi && hasDishWasher && hasParking && hasWasher && hasElevator && hasConditioner;
+    });
+  };
+
+  formFiltersElement.addEventListener('change', function (evt) {
+    var selectedFilter = evt.target;
+
+    window.map.hideOffersOnMap();
+    window.map.hideOfferInfo();
+
+    if (typeof onFilterChangeExternal === 'function') {
+      onFilterChangeExternal();
+    }
+
+    switch (selectedFilter.getAttribute('id')) {
+      case 'housing-type':
+        typeFilterValue = selectedFilter.value;
+        break;
+      case 'housing-price':
+        priceFilterValue = selectedFilter.value;
+        break;
+      case 'housing-rooms':
+        roomsFilterValue = selectedFilter.value;
+        break;
+      case 'housing-guests':
+        guestsFilterValue = selectedFilter.value;
+        break;
+      case 'filter-wifi':
+        wifiFilterValue = selectedFilter.checked;
+        break;
+      case 'filter-dishwasher':
+        dishwasherFilterValue = selectedFilter.checked;
+        break;
+      case 'filter-parking':
+        parkingFilterValue = selectedFilter.checked;
+        break;
+      case 'filter-washer':
+        washerFilterValue = selectedFilter.checked;
+        break;
+      case 'filter-elevator':
+        elevatorFilterValue = selectedFilter.checked;
+        break;
+      case 'filter-conditioner':
+        conditionerFilterValue = selectedFilter.checked;
+        break;
+    }
+  });
+
+  window.filter = {
+    apply: applyFilter,
+    setCallback: setCallback
+  };
+
+})();
--- js/map.js
+++ js/map.js
@@ -200,101 +200,15 @@
   window.form.updateAddress(initialPinX, initialPinY);
   window.backend.load(succesLoadDataHandler, window.notification.showError);
 
-  // filters
-  var formFiltersElement = document.querySelector('.map__filters');
-  var typeFilterValue = 'any';
-  var priceFilterValue = 'any';
-  var roomsFilterValue = 'any';
-  var guestsFilterValue = 'any';
-  var wifiFilterValue = false;
-  var dishwasherFilterValue = false;
-  var parkingFilterValue = false;
-  var washerFilterValue = false;
-  var elevatorFilterValue = false;
-  var conditionerFilterValue = false;
-
-  var Price = {
-    LOW: 10000,
-    HIGH: 50000
-  };
-
-  formFiltersElement.addEventListener('change', function (evt) {
-    var selectedFilter = evt.target;
-
-    switch (selectedFilter.getAttribute('id')) {
-      case 'housing-type':
-        typeFilterValue = selectedFilter.value;
-        break;
-      case 'housing-price':
-        priceFilterValue = selectedFilter.value;
-        break;
-      case 'housing-rooms':
-        roomsFilterValue = selectedFilter.value;
-        break;
-      case 'housing-guests':
-        guestsFilterValue = selectedFilter.value;
-        break;
-      case 'filter-wifi':
-        wifiFilterValue = selectedFilter.checked;
-        break;
-      case 'filter-dishwasher':
-        dishwasherFilterValue = selectedFilter.checked;
-        break;
-      case 'filter-parking':
-        parkingFilterValue = selectedFilter.checked;
-        break;
-      case 'filter-washer':
-        washerFilterValue = selectedFilter.checked;
-        break;
-      case 'filter-elevator':
-        elevatorFilterValue = selectedFilter.checked;
-        break;
-      case 'filter-conditioner':
-        conditionerFilterValue = selectedFilter.checked;
-        break;
-    }
-
-    var applyFilter = function (offers) {
-      return offers.filter(function (offer) {
-
-        var hasAppropiatePrice;
-        switch (priceFilterValue) {
-          case 'any':
-            hasAppropiatePrice = true;
-            break;
-          case 'low':
-            hasAppropiatePrice = offer.offer.price < Price.LOW;
-            break;
-          case 'middle':
-            hasAppropiatePrice = offer.offer.price >= Price.LOW && offer.offer.price <= Price.HIGH;
-            break;
-          case 'high':
-            hasAppropiatePrice = offer.offer.price > Price.HIGH;
-            break;
-        }
-
-        var hasAppropiateType = typeFilterValue === 'any' || offer.offer.type.toString() === typeFilterValue;
-        var hasAppropiateRooms = roomsFilterValue === 'any' || offer.offer.rooms.toString() === roomsFilterValue;
-        var hasAppropiateGuests = guestsFilterValue === 'any' || offer.offer.guests.toString() === guestsFilterValue;
-        var hasWiFi = wifiFilterValue === false || offer.offer.features.indexOf('wifi') !== -1;
-        var hasDishWasher = dishwasherFilterValue === false || offer.offer.features.indexOf('dishwasher') !== -1;
-        var hasParking = parkingFilterValue === false || offer.offer.features.indexOf('parking') !== -1;
-        var hasWasher = washerFilterValue === false || offer.offer.features.indexOf('washer') !== -1;
-        var hasElevator = elevatorFilterValue === false || offer.offer.features.indexOf('elevator') !== -1;
-        var hasConditioner = conditionerFilterValue === false || offer.offer.features.indexOf('conditioner') !== -1;
-
-        return hasAppropiateType && hasAppropiatePrice && hasAppropiateRooms && hasAppropiateGuests && hasWiFi && hasDishWasher && hasParking && hasWasher && hasElevator && hasConditioner;
-
-      });
-    };
-
-    var filtered = applyFilter(loadedOffers);
-    console.log(filtered);
-
+  window.filter.setCallback(function () {
+    var filteredPins = renderOffers(window.filter.apply(loadedOffers));
+    showOffersOnMap(filteredPins);
   });
 
   window.map = {
     disablePage: disablePage,
     activatePage: activatePage,
+    hideOffersOnMap: hideOffersOnMap,
+    hideOfferInfo: hideOfferInfo
   };
 })();
